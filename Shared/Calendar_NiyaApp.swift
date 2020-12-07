932e1e6544bb77252e121cd2f1ed4118a7c403a3
Niya0515 <921726627@qq.com>
2020-12-07 12:32:51 +0800

    Calendar_Niya

---
+++ Shared/Calendar_NiyaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calendar_NiyaApp.swift
+//  Shared
+//
+//  Created by 郑贝妮 on 2020/12/7.
+//
+
+import SwiftUI
+
+@main
+struct Calendar_NiyaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
